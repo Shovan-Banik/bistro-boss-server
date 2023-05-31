c64475d503f8685452cce849d9d72ff070a2bbb3
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-05-31 13:40:46 +0600

    axios inceptor done

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const express=require('express');
+const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app=express();
-const cors=require('cors');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT=(req,res,next)=>{
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true, message: 'unauthorized access'});
+  }
+  // bearer token
+  const token=authorization.split(' ')[1];
+
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true, message: 'unauthorized access'})
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fus4ier.mongodb.net/?retryWrites=true&w=majority`
+
+// var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-z719w2d-shard-00-00.fus4ier.mongodb.net:27017,ac-z719w2d-shard-00-01.fus4ier.mongodb.net:27017,ac-z719w2d-shard-00-02.fus4ier.mongodb.net:27017/?ssl=true&replicaSet=atlas-k7lhl0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,40 +47,100 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection=client.db('bistroDB').collection('menu');
-    const reviewCollection=client.db('bistroDB').collection('reviews');
-    const cartCollection=client.db('bistroDB').collection('carts');
+    const usersCollection = client.db('bistroDB').collection('users');
+    const menuCollection = client.db('bistroDB').collection('menu');
+    const reviewCollection = client.db('bistroDB').collection('reviews');
+    const cartCollection = client.db('bistroDB').collection('carts');
 
-    app.get('/menu',async(req,res)=>{
-        const result=await menuCollection.find().toArray();
-        res.send(result);
+
+    // create token
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+      console.log(token);
+      res.send({ token })
     })
-    app.get('/reviews',async(req,res)=>{
-        const result=await reviewCollection.find().toArray();
-        res.send(result);
+
+    // users related api
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existing user', existingUser);
+      if (existingUser) {
+        return res.send({ message: ' user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // menu related api
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
+    // review related api
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
     // cart collection api's
 
-    app.get('/carts',async(req,res)=>{
-      const email=req.query.email;
-      if(!email){
+    app.get('/carts', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
         res.send([]);
       }
-      const query= {email: email}
-      const result= await cartCollection.find(query).toArray();
+
+      const decodedEmail=req.decoded.email;
+      if(email !==decodedEmail){
+        return res.status(403).send({error:true, message: 'forbidden access'});
+      }
+
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.post('/carts',async(req,res)=>{
-      const item=req.body;
-      const result=await cartCollection.insertOne(item);
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
       res.send(result);
     })
 
-    app.delete('/carts/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id: new ObjectId(id)}
-      const result= await cartCollection.deleteOne(query);
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -79,10 +159,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('boss is sitting');
+app.get('/', (req, res) => {
+  res.send('boss is sitting');
 })
 
-app.listen(port,()=>{
-    console.log(`Bistro boss is sitting on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port: ${port}`);
 })
